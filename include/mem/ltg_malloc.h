7260a1b7ad99c3d62628dcbb27f4761986de0e4e
JK Wang <81369458@qq.com>
2019-08-16 14:30:17 +0800

    update for malloc

--- include/mem/ltg_malloc.h
+++ include/mem/ltg_malloc.h
@@ -1,10 +1,11 @@
 #ifndef __MALLOC_H__
 #define __MALLOC_H__
 
-extern int ltg_malloc(void **ptr, size_t size);
-extern int ltg_malign(void **_ptr, size_t align, size_t size);
-extern int ltg_realloc(void **_ptr, size_t size, size_t newsize);
-extern int ltg_free(void **ptr);
+void *ltg_malloc1(size_t size);
+int ltg_malloc(void **ptr, size_t size);
+int ltg_malign(void **_ptr, size_t align, size_t size);
+int ltg_realloc(void **_ptr, size_t size, size_t newsize);
+int ltg_free(void **ptr);
 
 int huge_mem_alloc1(void **_ptr, size_t size);
 void huge_mem_free1(void **ptr);
